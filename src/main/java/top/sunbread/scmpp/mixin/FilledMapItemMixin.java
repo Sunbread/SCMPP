a7c27d1f4f938c92c4ea161e75279101c8dbdd60
Sunbread <sunbread233@gmail.com>
2021-12-05 12:02:02 +0800

    Initial commit

---
+++ src/main/java/top/sunbread/scmpp/mixin/FilledMapItemMixin.java
@@ -0,0 +1,32 @@
+package top.sunbread.scmpp.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.NetworkSyncedItem;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.sunbread.scmpp.Scmpp;
+
+import java.util.Objects;
+
+@Mixin(FilledMapItem.class)
+public abstract class FilledMapItemMixin extends NetworkSyncedItem {
+
+    public FilledMapItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(at = @At(value = "HEAD"), method = "inventoryTick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/Entity;IZ)V")
+    public void onInventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected, CallbackInfo ci) {
+        if (!world.isClient && world instanceof ServerWorld serverWorld && entity instanceof ServerPlayerEntity player &&
+                (selected || player.getOffHandStack() == stack) &&
+                Objects.equals(FilledMapItem.getMapId(stack), Scmpp.MAP_ID)) Scmpp.noticePlayer(player, serverWorld);
+    }
+
+}
---
+++ src/main/java/top/sunbread/scmpp/mixin/ServerWorldMixin.java
@@ -0,0 +1,38 @@
+package top.sunbread.scmpp.mixin;
+
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.item.map.MapState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import top.sunbread.scmpp.Scmpp;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+@Mixin(ServerWorld.class)
+public abstract class ServerWorldMixin extends World {
+
+    protected ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DimensionType dimensionType, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed) {
+        super(properties, registryRef, dimensionType, profiler, isClient, debugWorld, seed);
+    }
+
+    @Inject(at = @At(value = "HEAD"), method = "getMapState(Ljava/lang/String;)Lnet/minecraft/item/map/MapState;", cancellable = true)
+    public void onGetMapState(String id, CallbackInfoReturnable<MapState> cir) {
+        if (Objects.equals(id, FilledMapItem.getMapName(Scmpp.MAP_ID))) cir.setReturnValue(null);
+    }
+
+    @Inject(at = @At(value = "HEAD"), method = "putMapState(Ljava/lang/String;Lnet/minecraft/item/map/MapState;)V", cancellable = true)
+    public void onPutMapState(String id, MapState state, CallbackInfo ci) {
+        if (Objects.equals(id, FilledMapItem.getMapName(Scmpp.MAP_ID))) ci.cancel();
+    }
+
+}
