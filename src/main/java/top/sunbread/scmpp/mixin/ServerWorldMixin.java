df1d21d3954f5e7dd0cdfcdfde6bafa89897d85f
Sunbread <sunbread233@gmail.com>
2021-12-07 23:08:25 +0800

    Remove redundant @Unique dependencies

--- src/main/java/top/sunbread/scmpp/mixin/ServerWorldMixin.java
+++ src/main/java/top/sunbread/scmpp/mixin/ServerWorldMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
